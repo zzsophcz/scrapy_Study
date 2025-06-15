ee6118543d7cb8ae6ad91f8052b7a802faebdb19
zzsophcz <zzsophcz@gmail.com>
2025-06-16 03:10:57 +0800

    优化：对已经爬取过的内容进行存储.txt，下次运行爬虫程序时会检查

--- pixiv_pic/pixiv_pic/spiders/pixiv_pic_spider.py
+++ pixiv_pic/pixiv_pic/spiders/pixiv_pic_spider.py
@@ -2,6 +2,18 @@ import scrapy
 import pickle
 from pixiv_pic.items import PixivPicItem
 
+#不好定义在类内，因为类内参数第一个是self，在类内会导致参数传递出问题
+def save_processed_link(link, file_path='processed_links.txt'):
+    with open(file_path, 'a', encoding='utf-8') as f:
+        f.write(link + '\n')
+
+def load_processed_links(file_path='processed_links.txt'):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return set(line.strip() for line in f if line.strip())
+    except FileNotFoundError:
+        return set()
+
 class PixivPicSpiderSpider(scrapy.Spider):
     name = "pixiv_pic_spider"
     allowed_domains = ["pixiv.net","i.pximg.net"]#添加新域名以便访问图片
@@ -21,22 +33,30 @@ class PixivPicSpiderSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         #表示在自定义前，先调用父类的初始化方法，保证 Scrapy 能正常工作。
         super().__init__(*args, **kwargs)
-        self.visited_urls = set()  # 记录访问过的页面防止死循环
+        self.processed_links = load_processed_links()
 
     def parse(self, response):
-
 
         print(response.url)
         # works=response.xpath('//ul[contains(@class,"sc-bf8cea3f-1")]/li[1]/div/div[2]/a/@href').extract_first()
         works = response.xpath('//ul[contains(@class,"sc-bf8cea3f-1")]/li')
         print(len(works))
         for work in works:
-            item={}
-            item["link"]=response.urljoin(work.xpath('./div/div[2]/a/@href').extract_first())
+            relative_url=response.urljoin(work.xpath('./div/div[2]/a/@href').extract_first())
+            # 如果链接已处理，跳过
+            if relative_url in self.processed_links:
+                print("已经处理过该链接，跳过节省时间")
+                continue
+
+            # 否则，加入记录并继续处理
+            self.processed_links.add(relative_url)
+            save_processed_link(relative_url)
+
             # print(item["link"])
-            #获取图片url字典
-            yield scrapy.Request(url=item["link"], callback=self.parse_detail, meta={"selenium": 'pic'},
+            yield scrapy.Request(url=relative_url, callback=self.parse_detail, meta={"selenium": 'pic'},
                                  cookies=response.request.cookies)
+        print("如果这段消息出现在最后，说明链接都已经是处理过的了")
+
     def parse_detail(self, response):
         # pass
         print("作品详情页面"+response.url)
@@ -49,19 +69,6 @@ class PixivPicSpiderSpider(scrapy.Spider):
             item['author'] = author
             yield item
 
-            # yield scrapy.Request(
-            #     url=pic_url,
-            #     headers={
-            #         'Referer': 'https://www.pixiv.net/',  # 必须加 Referer
-            #     },
-            #     callback=self.save_image
-            # )
-
-    def save_image(self, response):
-            image_name = response.url.split('/')[-1]
-            with open(f'pixiv_images/{image_name}', 'wb') as f:
-                f.write(response.body)
-                print(f"[+] 图片已保存为 {image_name}")
 
 
 
